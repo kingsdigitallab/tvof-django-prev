a5285d239914158a93a34803dd24c22694de9e97
Neil Jakeman <neil.jakeman@kcl.ac.uk>
2016-01-07 09:44:01 +0000

    First commit

---
+++ tvof/settings/dev.py
@@ -0,0 +1,58 @@
+from base import *
+
+DEBUG = True
+
+INTERNAL_IPS = INTERNAL_IPS + ('', )
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'app_tvof_dev',
+        'USER': 'app_tvof',
+        'PASSWORD': '',
+        'HOST': ''
+    },
+}
+
+LOGGING_LEVEL = logging.DEBUG
+
+LOGGING['loggers']['tvof']['level'] = LOGGING_LEVEL
+
+TEMPLATES[0]['OPTIONS']['debug'] = True
+
+# -----------------------------------------------------------------------------
+# Django Extensions
+# http://django-extensions.readthedocs.org/en/latest/
+# -----------------------------------------------------------------------------
+
+try:
+    import django_extensions
+
+    INSTALLED_APPS = INSTALLED_APPS + ('django_extensions',)
+except ImportError:
+    pass
+
+# -----------------------------------------------------------------------------
+# Django Debug Toolbar
+# http://django-debug-toolbar.readthedocs.org/en/latest/
+# -----------------------------------------------------------------------------
+
+try:
+    import debug_toolbar
+
+    INSTALLED_APPS = INSTALLED_APPS + ('debug_toolbar',)
+    MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + (
+        'debug_toolbar.middleware.DebugToolbarMiddleware',)
+    DEBUG_TOOLBAR_PATCH_SETTINGS = True
+except ImportError:
+    pass
+
+# -----------------------------------------------------------------------------
+# Local settings
+# -----------------------------------------------------------------------------
+
+try:
+    from local import *
+except ImportError:
+    print('failed to import local settings')
+    raise ImportError('Error importing local settings')
